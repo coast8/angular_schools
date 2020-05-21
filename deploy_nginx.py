cc12682a9d56f72a941af1fe52fa02917057f5b7
Coast8 <smythy.costa@gmail.com>
2020-05-21 09:28:16 -0300

    deploy

---
+++ deploy_nginx.py
@@ -0,0 +1,11 @@
+
+
+
+# reference
+https://www.devdungeon.com/content/deploy-angular-apps-nginx
+
+
+# dcoker 
+docker run -i -t --name some-nginx -p 8080:80 -v /c/Users/asmythy/docker-files/vol-nginx:/usr/share/nginx/html nginx /bin/bash
+
+
